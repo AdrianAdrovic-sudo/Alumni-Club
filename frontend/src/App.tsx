18b47278a693cb21b9ff02ee261940397613faf8
Mia Vranes <miavranes4@gmail.com>
2025-11-27 01:12:18 +0100

    Added Theseses page and MyProfile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,7 @@ import AboutUs from "./pages/AboutUs.tsx";
 import Contact from "./pages/Contact.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import Theses from "./pages/Theses.tsx";
-import Messages from './components/messages/Messages';
-import { AuthProvider, useAuth } from "./context/AuthContext";
-import PublicRoute from "./components/common/PublicRoute";
-import AdminRoute from "./components/common/AdminRoute";
+
 
 export default function App() {
   return (
@@ -26,34 +23,15 @@ export default function App() {
             <Route path="/Home" element={<Home />} />
             <Route path="/AlumniDirectory" element={<AlumniDirectory />} />
             <Route path="/Blog" element={<Blog />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/AboutUs" element={<AboutUs/>} />
+            <Route path="/Contact" element={<Contact/>} />
+            <Route path="/Theses" element={<Theses/>} />
             
-            {/* Protect login route - redirect if already authenticated */}
-            <Route 
-              path="/login" 
-              element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              } 
-            />
-            
-            <Route path="/AboutUs" element={<AboutUs />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/Theses" element={<Theses />} />
-            <Route path="/messages" element={<Messages />} />
 
-            <Route
-              path="/Dashboard"
-              element={
-                <AdminRoute>
-                  <Dashboard />
-                </AdminRoute>
-              }
-            />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </AuthProvider>
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
